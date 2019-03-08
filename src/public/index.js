e41f2a8899a2394abd74a8b01dd03030f7a268a1
kolesan <kolesan90@gmail.com>
2019-03-08 15:55:05 +0200

    Treat response as objects containing urls not just urls

--- src/public/index.js
+++ src/public/index.js
@@ -5,21 +5,22 @@ import photoProvider, { Sizes } from './services/Photos';
 
 let list = document.querySelector("#list");
 
-const receivedPhotosPromise = photoProvider.getMultiple(5, Sizes.m);
+const receivedPhotosPromise = photoProvider.getMultiple(10, Sizes.m);
 
 receivedPhotosPromise
   .then(resp => {
-    let photoUrls = resp.data;
-    console.log(photoUrls);
-    photoUrls.forEach(url => {
+    let photos = resp.data;
+    console.log(photos);
+    photos.forEach(photo => {
       list.appendChild(
         element({
           tag: "li",
+          classes: "list__item",
           children: [
             element({
               tag: 'img',
               attributes: {
-                src: url
+                src: photo.url
               }
             })
           ]
